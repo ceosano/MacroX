3eb5b6d1919e5352f3921c4524b691071ef39635
Nnaji Victor <nnaji.victorr001@gmail.com>
2024-07-29 15:57:48 +0100

    implemented global record and play

--- preload.js
+++ preload.js
@@ -1,14 +1,17 @@
-const { contextBridge, ipcRenderer } = require('electron');
+const { contextBridge, ipcRenderer } = require("electron");
 
-contextBridge.exposeInMainWorld('electronAPI', {
-  recordAction: (action) => ipcRenderer.send('record-action', action),
-  playActions: () => ipcRenderer.send('play-actions'),
-  saveActions: (actions) => ipcRenderer.send('save-actions', actions),
-  onUpdateActions: (callback) => ipcRenderer.on('update-actions', callback),
-  deleteAction: (actionId) => ipcRenderer.send('delete-action', actionId),
+contextBridge.exposeInMainWorld("electronAPI", {
+  recordAction: (action) => ipcRenderer.send("record-action", action),
+  playActions: () => ipcRenderer.send("play-actions"),
+  saveActions: (actions) => ipcRenderer.send("save-actions", actions),
+  onUpdateActions: (callback) => ipcRenderer.on("update-actions", callback),
+  deleteAction: (actionId) => ipcRenderer.send("delete-action", actionId),
   callSwitchFunction: async (flag) => {
-    const result = await ipcRenderer.invoke('call-switch-function', flag);
+    const result = await ipcRenderer.invoke("call-switch-function", flag);
     return result;
   },
-  onKeyPressed: (callback) => ipcRenderer.on('key-pressed', (_event, value) => callback(value)),
+  onKeyPressed: (callback) =>
+    ipcRenderer.on("key-pressed", (_event, value) => callback(value)),
+  registerShortcut: (shortcut) =>
+    ipcRenderer.send("register-shortcut", shortcut),
 });
--- renderer.js
+++ renderer.js
@@ -1,11 +1,13 @@
-const toggleRecordButton = document.getElementById('toggle-record-button'); // Change to a single toggle button
+const toggleRecordButton = document.getElementById("toggle-record-button"); // Change to a single toggle button
 
 toggleRecordButton.disabled = false;
 let isRecording = false; // State to track recording status
 
-toggleRecordButton.addEventListener('click', async () => {
+toggleRecordButton.addEventListener("click", async () => {
   isRecording = !isRecording; // Toggle the recording state
-  toggleRecordButton.textContent = isRecording ? 'Stop Recording (ctrl + r)' : 'Start Recording (ctrl + r)'; // Update button text based on state
+  toggleRecordButton.textContent = isRecording
+    ? "Stop Recording (ctrl + r)"
+    : "Start Recording (ctrl + r)"; // Update button text based on state
   if (isRecording) {
     recordStartTime = performance.now();
     // Start recording
@@ -17,64 +19,48 @@ toggleRecordButton.addEventListener('click', async () => {
   }
 });
 
-const playButton = document.getElementById('play-button');
-const actionsBody = document.getElementById('actions-body');
-const keyInput = document.getElementById('key-input');
-const bindKeyButton = document.getElementById('bind-key-button');
+const playButton = document.getElementById("play-button");
+const actionsBody = document.getElementById("actions-body");
+const keyInput = document.getElementById("key-input");
+const bindKeyButton = document.getElementById("bind-key-button");
 
 let recordStartTime = 0;
 let updatedActions = [];
 let isKeyBound = false; // Variable to indicate if a key is bound
 
-document.addEventListener('keydown', async function (event) {
-  if (event.ctrlKey && event.key === 's') {
-    event.preventDefault(); // Prevent the default action of Ctrl + S (usually save)
-    isRecording = true;
-    toggleRecordButton.textContent = 'Stop Recording';
-    recordStartTime = performance.now();
-    await window.electronAPI.callSwitchFunction(true);
-  } else if (event.ctrlKey && event.key === 't') {
-    event.preventDefault(); // Prevent the default action of Ctrl + S (usually save)
-    isRecording = false;
-    toggleRecordButton.textContent = 'Start Recording';
-    startRecordButton.disabled = false;
-    stopRecordButton.disabled = true;
-    isKeyBound = false;
-    await window.electronAPI.callSwitchFunction(false);
-    window.electronAPI.saveActions(updatedActions);
-  }
-});
-
-playButton.addEventListener('click', () => {
-  window.electronAPI.playActions();
+playButton.addEventListener("click", async () => {
+  await window.electronAPI.playActions();
 });
 
 // Step 1: Capture the Key Input
-let bindKey = ''; // Variable to store the key to bind
-keyInput.addEventListener('keydown', function (event) {
+let bindKey = ""; // Variable to store the key to bind
+keyInput.addEventListener("keydown", function (event) {
   isKeyBound = false; // Reset isKeyBound to false when a new key is pressed
   const isLetter = /^[a-zA-Z]$/; // Regular expression to check if the key is a letter
   if (isLetter.test(event.key)) {
     event.preventDefault(); // Prevent default to not type in input
     bindKey = event.key; // Store the pressed key
-    document.getElementById('key-input').value = bindKey; // Show the pressed key in the input field
+    document.getElementById("key-input").value = bindKey; // Show the pressed key in the input field
   }
 });
 
 // Step 2: Bind the Key to the Button
-bindKeyButton.addEventListener('click', function () {
+bindKeyButton.addEventListener("click", function () {
   // Inform the user which key is bound, or handle the binding process
-  if (bindKey !== '') { // Check if bindKey is not empty
+  if (bindKey !== "") {
+    // Check if bindKey is not empty
+    console.log("==>", bindKey);
     isKeyBound = true; // Set isKeyBound to true indicating a key has been bound
     // You can add additional logic here if needed
+    window.electronAPI.registerShortcut(bindKey);
   } else {
     isKeyBound = false;
   }
 });
 
 // Step 3: Replace Play Button Click with Key Press
-document.addEventListener('keydown', function (event) {
-  if (bindKey != '' && isKeyBound && event.key === bindKey && !event.repeat) {
+document.addEventListener("keydown", function (event) {
+  if (bindKey != "" && isKeyBound && event.key === bindKey && !event.repeat) {
     event.preventDefault(); // Prevent default action of the key
     // Trigger the Play button's functionality
     // document.getElementById('play-button').click();
@@ -83,10 +69,10 @@ document.addEventListener('keydown', function (event) {
 });
 
 // Attach a single event listener to the parent element for event delegation
-actionsBody.addEventListener('click', function (event) {
+actionsBody.addEventListener("click", function (event) {
   // Check if the clicked element is a delete button
-  if (event.target.classList.contains('delete-button')) {
-    const actionId = event.target.getAttribute('data-action-id');
+  if (event.target.classList.contains("delete-button")) {
+    const actionId = event.target.getAttribute("data-action-id");
     window.electronAPI.deleteAction(actionId);
     // Here you can add your logic to delete the action
     // For example, remove the row from the DOM, update the actions array, etc.
@@ -97,7 +83,7 @@ function handleMouseMove(event) {
   const now = performance.now();
   const action = {
     id: now,
-    type: 'mouse',
+    type: "mouse",
     x: event.clientX,
     y: event.clientY,
     delay: now - recordStartTime,
@@ -107,16 +93,26 @@ function handleMouseMove(event) {
 }
 
 function updateActionsList(action) {
-  const row = document.createElement('tr');
+  const row = document.createElement("tr");
   row.innerHTML = `
     <td><div class="cell-wrapper">${action.type}</div></td>
-    <td><div class="cell-wrapper ${!action.x || action.x === "" ? "none-background" : ""}">${action.x || ''}</div></td>
-    <td><div class="cell-wrapper ${!action.y || action.y === "" ? "none-background" : ""}">${action.y || ''}</div></td>
-    <td><div class="cell-wrapper ${!action.key || action.key === "" ? "none-background" : ""}">${action.key || ''}</div></td>
-    <td><div class="cell-wrapper ${!action.state || action.state === "" ? "none-background" : ""}">${action.button || ''} ${action.state || ''}</div></td>
+    <td><div class="cell-wrapper ${
+      !action.x || action.x === "" ? "none-background" : ""
+    }">${action.x || ""}</div></td>
+    <td><div class="cell-wrapper ${
+      !action.y || action.y === "" ? "none-background" : ""
+    }">${action.y || ""}</div></td>
+    <td><div class="cell-wrapper ${
+      !action.key || action.key === "" ? "none-background" : ""
+    }">${action.key || ""}</div></td>
+    <td><div class="cell-wrapper ${
+      !action.state || action.state === "" ? "none-background" : ""
+    }">${action.button || ""} ${action.state || ""}</div></td>
     <td><div class="cell-wrapper">${action.delay.toFixed(2)}</div></td>
     <td>
-      <button class="delete-button" data-action-id="${action.id}">Delete</button>
+      <button class="delete-button" data-action-id="${
+        action.id
+      }">Delete</button>
     </td>
   `;
   actionsBody.appendChild(row);
@@ -125,12 +121,12 @@ function updateActionsList(action) {
 window.electronAPI.onUpdateActions((event, actions) => {
   try {
     updatedActions = actions;
-    actionsBody.innerHTML = '';
+    actionsBody.innerHTML = "";
     for (const action of actions) {
       updateActionsList(action);
     }
   } catch (error) {
-    console.error('Error updating actions:', error);
+    console.error("Error updating actions:", error);
   }
 });
 
@@ -139,9 +135,9 @@ window.electronAPI.onKeyPressed((event) => {
   const now = performance.now();
   const type = event._raw.split(",")[0].toLowerCase();
   const key = event.name;
-  let state = '';
-  let button = '';
-  if (type === 'keyboard') {
+  let state = "";
+  let button = "";
+  if (type === "keyboard") {
     state = `key ${event.state.toLowerCase()}`;
     action = {
       id: now,
@@ -150,7 +146,7 @@ window.electronAPI.onKeyPressed((event) => {
       state: state,
       delay: now - recordStartTime,
     };
-  } else if (type === 'mouse') {
+  } else if (type === "mouse") {
     state = event.state.toLowerCase();
     button = event.name.split(" ").pop().toLowerCase();
     action = {
@@ -159,26 +155,10 @@ window.electronAPI.onKeyPressed((event) => {
       x: event.location[0],
       y: event.location[1],
       button: button,
-      state: 'up',
+      state: "up",
       delay: now - recordStartTime,
     };
   }
-  // console.log(action);
   window.electronAPI.recordAction(action);
   updateActionsList(action);
-});
-
-const { ipcRenderer } = require('electron');
-
-ipcRenderer.on('toggle-recording', async (event, isRecording) => {
-    if (isRecording) {
-        // Start recording
-        await window.electronAPI.callSwitchFunction(true);
-        toggleRecordButton.textContent = 'Stop Recording';
-    } else {
-        // Stop recording
-        await window.electronAPI.callSwitchFunction(false);
-        toggleRecordButton.textContent = 'Start Recording';
-        window.electronAPI.saveActions(updatedActions);
-    }
 });
